3efff9f5970d6a60abc29a437350f5f37ae7956f
M1000fr <formentel.remi@gmail.com>
2025-08-21 22:02:11 +0200

    refactor: make App class final to prevent subclassing

--- src/main/java/dev/corelia/core/App.java
+++ src/main/java/dev/corelia/core/App.java
@@ -2,7 +2,7 @@ package dev.corelia.core;
 
 import java.nio.file.Path;
 
-public class App {
+public final class App {
     public static void main(String[] args) throws Exception {
         Path pluginsDir = Path.of("plugins");
         PluginManager pm = new PluginManager(pluginsDir);
